9e4c0aa3ad263e52783c6582901cc0b6f4c1eca5
Rogelio <roglavsil21@gmail.com>
2022-04-20 23:47:12 -0500

    Se agrega el código para piedra_papel_tijeras

--- src/Piedra_Papel_Tijeras.py
+++ src/Piedra_Papel_Tijeras.py
@@ -20,23 +20,57 @@ ARGUMENTS
 SEE ALSO
         none
 """
-# Definir opciones validas.
+#Importar la libreria necesaria para aleatoriedad.
+import random
 
-# El usuario define su opción.
+# Definir opciones validas y el usuario selecciona su opción.
+print("Bienvenido, por favor introduce tu nombre para empezar a jugar.")
+nom_usuario= input()
+print("Elija su opcion: piedra, papel o tijeras")
+usuario= input().lower()
+
 
 # La computadora elige su opción.
+opcion_cpu= random.randint(1,3)
+if opcion_cpu == 1:
+        computadora= "piedra"
+elif opcion_cpu == 2:
+        computadora= "papel"
+else:
+        computadora= "tijeras"
+        
+#Mostramos ambas elecciones.
+print("La computadora eligio:", computadora)
+print("Tu eleccion fue:", usuario)
 
 #Definimos todos los casos.
 # Igualdad es empate.
+if usuario == computadora:
+        print("Empate. No hay un ganador")
 
 #Si el usuario eligio tijera.
 # Tijera gana a papel. Usuario gana.
 # Tijera pierde contra piedra. Computadora gana.
+if usuario == "tijeras":
+        if computadora == "papel":
+                print("Felicidades has ganado", nom_usuario)
+        elif computadora == "piedra": 
+                print("Has perdido", nom_usuario)
 
 #Si el usuario eligio papel.
 # Papel gana a la piedra. Usuario gana.
 # Papel pierde contra tijeras. Computadora gana.
+if usuario == "papel":
+        if computadora == "piedra":
+                print("Felicidades has ganado", nom_usuario)
+        elif computadora == "tijeras": 
+                print("Has perdido", nom_usuario)
 
 #Si el usuario eligio papel.
 # Piedra gana a tijeras. Usuario gana.
 # Piedra pierde contra papel. Computadora gana.
+if usuario == "piedra":
+        if computadora == "tijeras":
+                print("La buena piedra nada le gana, felicitaciones", nom_usuario)
+        elif computadora == "papel": 
+                print("Has perdido", nom_usuario)
