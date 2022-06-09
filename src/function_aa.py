f055ef52510deed07dd203092abe8e5225b7dbcb
Rogelio <roglavsil21@gmail.com>
2022-06-09 02:26:48 -0500

    Se añade la version 2.0 del programa

--- src/function_aa.py
+++ src/function_aa.py
@@ -5,14 +5,14 @@ NAME
       
 VERSION
 
-        1.0
+        2.0
 AUTHOR
 
         Rogelio Avila
         
 DESCRIPTION
 
-        Se toma como input una secuencia de aminoacidos dada por el usuario y un aminoacido cuyo
+        Se toma como input una secuencia de aminoacidos dada por el usuario y aminoacidos cuyo
         porcentaje dentro de esa secuencia es retornado al usuario.
        
 CATEGORY
@@ -25,57 +25,83 @@ USAGE
         
 ARGUMENTS
         
-    positional arguments:
-    
-        secuencia   Secuencia de aminoacidos a leer.
-        aminoacido  Aminoacido a calcular su porcentaje.
-
-    options:
-
-        -h, --help  show this help message and exit
+        options:
+        -h, --help            show this help message and exit
+        
+        -s SECUENCIA, --secuencia 
+                                Path archivo con la secuencia de aminoacidos a leer.
+                                
+        -a AMINOACIDOS, --aminoacidos 
+                                Aminoacidos a calcular. Default: aminoacidos hidrofobicos.
+                                
+        -d DECIMALS, --decimals 
+                                Decimales para redondear el porcentaje. Default: 2.
 
 SEE ALSO
 
         None
 
 """
+# ===========================================================================
+# =                            imports
+# ===========================================================================
+
 import argparse
+import aminoacid_counter
 
-aa_parser= argparse.ArgumentParser(description="Script para calcular el porcentaje de un aminoácido en una secuencia.")
+# ===========================================================================
+# =                            main
+# ===========================================================================
+
+
+# Se crea un codigo de error para el primer argumento.
+class ValueError2(Exception):
+    pass
+
+
+aa_parser = argparse.ArgumentParser(
+    description="Script para calcular el porcentaje de un aminoácido en una secuencia.")
 # Crear los argumentos que recibirá el programa para funcionar.
-aa_parser.add_argument("secuencia", help="Secuencia de aminoacidos a leer.")
-aa_parser.add_argument("aminoacido", help="Aminoacido a calcular su porcentaje.")
+aa_parser.add_argument("-s", "--secuencia",
+                       help="Path archivo con la secuencia de aminoacidos a leer.", required=True,)
 
-arguments= aa_parser.parse_args()
+aa_parser.add_argument("-a", "--aminoacidos",
+                       help="Aminoacidos a calcular. Default: aminoacidos hidrofobicos.", required=False, type=str, default=['A', 'I', 'L', 'M', 'F', 'W', 'Y', 'V'])
 
+aa_parser.add_argument("-d", "--decimals",
+                       help="Decimales para redondear el porcentaje. Default: 2.", required=False, type=int, default=2)
+arguments = aa_parser.parse_args()
 
 try:
 
+    # Abrir el archivo y copiar su contenido a una variable.
+    archivo = open(arguments.secuencia)
+    contenido_archivo = archivo.read()
+    archivo.close()
+    secuencia = contenido_archivo.replace("\n", "")
     # Comprobar que el segundo argumento sea un aminoácido.
-    if arguments.aminoacido.upper() not in ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K',
-                    'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']:
-                    raise ValueError("El segundo argumento no es un aminoácido.")
-                    
-    # Comprobar que la secuencia contenga solo aminoácidos.
-    for aminoacido in arguments.secuencia:
+    for aminoacido in arguments.aminoacidos:
         if aminoacido.upper() not in ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K',
-                    'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']:
-                    raise IOError("La secuencia introducida no contiene solo aminoacidos.")
-                    
-    # Definir la función con nombre y parametros.                
-    def get_aa_percentage(sec, aminoacid):
-        # Realizamos todos los calculos para obtener el porcentaje.
-        length = len(sec)
-        amino_count = sec.upper().count(aminoacid.upper())
-        percentage = (amino_count/length)*100
-        return round(percentage, 2)
+                                      'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']:
+            raise ValueError(
+                "El segundo argumento no contiene solamente aminoacidos.")
 
-    # Imprimir el resultado de la función.
-    print("El porcentaje de este aminoacido dentro de la secuencia es:")
-    print(get_aa_percentage(arguments.secuencia, arguments.aminoacido),"%")
+    # Comprobar que la secuencia contenga solo aminoácidos.
+    for aminoacido in secuencia:
+        if aminoacido.upper() not in ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K',
+                                      'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']:
+            raise ValueError2(
+                "El primer argumento no contiene solamente aminoacidos.")
+
+    # Llamar a la funcion.
+    aminoacid_counter.get_aa_percentage(secuence=secuencia,
+                                        aminoacids=arguments.aminoacidos, decimals=arguments.decimals)
 
 # Se agregan errores por si el usuario se equivoca con los argumentos.
 except ValueError:
-    print("El segundo argumento no es un aminoácido.")
-except IOError:
-    print("La secuencia introducida no contiene solo aminoacidos.")
+    print("Error: ¡Caracter invalido en el segundo argumento!")
+except ValueError2:
+    print("Error: ¡Caracter invalido en el primer argumento!")
+except IOError as ex:
+    print(
+        f"Error: ¡El archivo {ex.filename} no se encuentra en la dirección indicada o no existe!")
