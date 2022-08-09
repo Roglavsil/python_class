9184fc24bed705cdc4340c810fe519e8015f6f47
Roglavsil <ravila@lcg.unam.mx>
2022-08-09 13:01:27 -0500

    Modificacion en el autor

--- src/porcentaje_AT_GC.py
+++ src/porcentaje_AT_GC.py
@@ -8,7 +8,7 @@ VERSION
         2.0
 AUTHOR
 
-        Rogelio Avila
+        Rogelio Avila Silva
         
 DESCRIPTION
 
@@ -38,37 +38,40 @@ SEE ALSO
 import argparse
 
 # Obtener el path al leer los argumentos.
-at_parser= argparse.ArgumentParser(description='Script que calcula el porcentaje de AT y GC de una secuencia.')
+at_parser = argparse.ArgumentParser(
+    description='Script que calcula el porcentaje de AT y GC de una secuencia.')
 
-at_parser.add_argument("input", metavar="path", help="Path al archivo con secuencias")
+at_parser.add_argument(
+    "-i", "--input", help="Path al archivo con secuencias", required=True)
 
-arguments= at_parser.parse_args()
+arguments = at_parser.parse_args()
 
 try:
-        #Abrir el archivo y copiar su contenido a una variable.
-        archivo=open(arguments.input)
-        contenido_archivo=archivo.read()
-        archivo.close()
+    # Abrir el archivo y copiar su contenido a una variable.
+    archivo = open(arguments.input)
+    contenido_archivo = archivo.read()
+    archivo.close()
 
-        # Imprimir los datos introducidos
-        print(f"La secuencia del archivo es: {contenido_archivo}\n")
+    # Imprimir los datos introducidos
+    print(f"La secuencia del archivo es: {contenido_archivo}\n")
 
-        # Quitar saltos de linea obtenemos la longitud de la secuencia
-        DNAseq = contenido_archivo.replace("\n", "")  
-        
-        #Comprobar que solo haya nucleotidos en la cadena.
-        for base in DNAseq:
-                if base not in ["A", "C", "G", "T"]:
-                        raise ValueError()
-        Totalnucleotidos= len(DNAseq)
+    # Quitar saltos de linea obtenemos la longitud de la secuencia
+    DNAseq = contenido_archivo.replace("\n", "")
 
-        #Calcular el porcentaje de AT y GC
-        DNA_AT= {((DNAseq.count("T") + DNAseq.count("A")) / Totalnucleotidos) * 100}
-        DNA_GC= {((DNAseq.count("G") + DNAseq.count("C")) / Totalnucleotidos) * 100}
+    # Comprobar que solo haya nucleotidos en la cadena.
+    for base in DNAseq:
+        if base not in ["A", "C", "G", "T"]:
+            raise ValueError()
+    Totalnucleotidos = len(DNAseq)
 
-        # Imprimir el resultado
-        print(f"Porcentaje de AT: {DNA_AT}%\nPorcentaje de GC: {DNA_GC}% ")
+    # Calcular el porcentaje de AT y GC
+    DNA_AT = {((DNAseq.count("T") + DNAseq.count("A")) / Totalnucleotidos) * 100}
+    DNA_GC = {((DNAseq.count("G") + DNAseq.count("C")) / Totalnucleotidos) * 100}
+
+    # Imprimir el resultado
+    print(f"Porcentaje de AT: {DNA_AT}%\nPorcentaje de GC: {DNA_GC}% ")
 except IOError as ex:
-        print(f"El archivo {ex.filename} no se encuentra en la dirección indicada o no existe.")
+    print(
+        f"El archivo {ex.filename} no se encuentra en la dirección indicada o no existe.")
 except ValueError:
     print("La secuencia no contiene solamente bases.")
